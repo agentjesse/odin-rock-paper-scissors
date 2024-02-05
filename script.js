598c43035e4fe2577b2a6fe21f47bb8b5dfea19d
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-05 01:56:41 -0500

    Added pseudocode for game
    also updated title in index.html

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // console.log('test')
+// ---------TIC TAC TOE --------------
+// goal is to make a tic tac toe game with input from a prompt to the user of a choice(rock/paper/scissors).
 
+//welcome player and ask to input rock/paper/scissors as their choice, assigning the value to a mutable variable. also start a score counter variable
+
+//generate a computer choice
+
+//compare the player vs computer's choices
+
+//alert player of the game result and score
+
+//restart game loop
